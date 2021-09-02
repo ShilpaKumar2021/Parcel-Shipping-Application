599db4c377e12cfad1cd7e2145cd3575b7ec71c4
Shilpa <87871367+ShilpaKumar2021@users.noreply.github.com>
2021-09-01 22:00:55 -0500

    Initialize AddressValidation

---
+++ Parcel Shipping Application/Test Suites/Results/addressValidationTS.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>addressValidationTS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>shilpa.kumar@smartwarehousing.com;</mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>84d3e6fe-4651-4620-b53a-7de49fdae0f0</testSuiteGuid>
+   <testCaseLink>
+      <guid>7e1030e3-5a21-47df-869b-a10bc2e1cb32</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Parcel Shipping/batchTC</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e010c13b-f682-4174-bf75-a28b13b6c56e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Parcel Shipping/addressValidationTC</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f90edd5b-d67e-419b-8a6f-6a2f990f0d62</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Parcel Shipping/labelsTC</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
